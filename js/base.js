2723750ab8257240e0d1fcbf8e85fe3107fe4b4e
cuiyuan <1023968712@qq.com>
2018-09-26 19:14:02 +0800

    崔源
    增加页面

---
+++ js/base.js
@@ -0,0 +1,3 @@
+var Length = document.documentElement.clientWidth;
+var baseWidth = Length <= 1024 ? Length : 1024 < Length ? 750 : '';
+document.documentElement.style.fontSize = baseWidth / 750 * 100 + 'px';
